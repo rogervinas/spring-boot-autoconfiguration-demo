b6793d9385d8fbddd6c563a99ebc725eef5672ba
Roger Viñas <roger.vinas@scmspain.com>
2018-07-05 18:15:44 +0200

    Spring Boot 2.x

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Inspired by [hello-service-auto-configuration](https://github.com/snicoll-demos/
 As stated in [Testing Auto-Configurations in Spring Boot 2.0](https://spring.io/blog/2018/03/07/testing-auto-configurations-with-spring-boot-2-0) support for Auto-Configuration test is better in Spring Boot 2.0 so:
 * **master** branch -> Spring Boot 2.x
 * **release/1.x** branch -> Spring Boot 1.x
+
+## Build & Test
+
+`./mvnw install`
+
+## Run
+
+`./mvnw spring-boot:run -pl foo-service-app`
--- foo-service-starter/src/test/java/com/rogervinas/foo/autoconfigure/FooServiceAutoConfigurationTest.java
+++ foo-service-starter/src/test/java/com/rogervinas/foo/autoconfigure/FooServiceAutoConfigurationTest.java
@@ -4,112 +4,117 @@ import com.rogervinas.foo.FooService;
 import com.rogervinas.foo.JsonFooService;
 import com.rogervinas.foo.XmlFooService;
 import java.io.PrintStream;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.util.EnvironmentTestUtils;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class FooServiceAutoConfigurationTest {
 
-  private AnnotationConfigApplicationContext context;
-
   private static PrintStream out = mock(PrintStream.class);
 
   @Before
   public void before() {
-    context = new AnnotationConfigApplicationContext();
     out = mock(PrintStream.class);
-  }
-
-  @After
-  public void after() {
-    if (context != null) {
-      context.close();
-    }
-    reset(out);
   }
 
   @Test
   public void should_autoConfigure_defaultFooService_with_defaultConfiguration() {
-    load(BaseConfiguration.class);
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("bar");
-
-    assertThat(bean).isInstanceOf(XmlFooService.class);
-    verify(out, times(1)).println("<foo>bar</foo>");
+    contextRunner(BaseConfiguration.class)
+        .run(context ->
+          assertThat(context)
+              .getBean(FooService.class)
+              .isInstanceOf(XmlFooService.class)
+              .satisfies(bean -> {
+                bean.doSomething("bar");
+                verify(out, times(1)).println("<foo>bar</foo>");
+              })
+        );
   }
 
   @Test
   public void should_autoConfigure_defaultFooService_with_userConfiguration() {
-    load(BaseConfiguration.class, "foo.xml.tagName=city");
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("barcelona");
-
-    assertThat(bean).isInstanceOf(XmlFooService.class);
-    verify(out, times(1)).println("<city>barcelona</city>");
+    contextRunner(BaseConfiguration.class, "foo.xml.tagName=city")
+        .run(context ->
+          assertThat(context)
+              .getBean(FooService.class)
+              .isInstanceOf(XmlFooService.class)
+              .satisfies(bean -> {
+                bean.doSomething("barcelona");
+                verify(out, times(1)).println("<city>barcelona</city>");
+              })
+        );
   }
 
   @Test
   public void should_autoConfigure_xmlFooService_with_userConfiguration() {
-    load(BaseConfiguration.class, "foo.xml.enabled=true", "foo.xml.tagName=fruit");
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("mango");
-
-    assertThat(bean).isInstanceOf(XmlFooService.class);
-    verify(out, times(1)).println("<fruit>mango</fruit>");
+    contextRunner(BaseConfiguration.class, "foo.xml.enabled=true", "foo.xml.tagName=fruit")
+        .run(context ->
+            assertThat(context)
+                .getBean(FooService.class)
+                .isInstanceOf(XmlFooService.class)
+                .satisfies(bean -> {
+                  bean.doSomething("mango");
+                  verify(out, times(1)).println("<fruit>mango</fruit>");
+                })
+        );
   }
 
   @Test
   public void should_autoConfigure_jsonFooService_with_defaultConfiguration() {
-    load(BaseConfiguration.class, "foo.json.enabled=true");
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("bar");
-
-    assertThat(bean).isInstanceOf(JsonFooService.class);
-    verify(out, times(1)).println("{ \"foo\" : \"bar\" }");
+    contextRunner(BaseConfiguration.class, "foo.json.enabled=true")
+        .run(context ->
+            assertThat(context)
+                .getBean(FooService.class)
+                .isInstanceOf(JsonFooService.class)
+                .satisfies(bean -> {
+                  bean.doSomething("bar");
+                  verify(out, times(1)).println("{ \"foo\" : \"bar\" }");
+                })
+        );
   }
 
   @Test
   public void should_autoConfigure_jsonFooService_with_userConfiguration() {
-    load(BaseConfiguration.class, "foo.json.enabled=true", "foo.json.fieldName=fruit");
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("orange");
-
-    assertThat(bean).isInstanceOf(JsonFooService.class);
-    verify(out, times(1)).println("{ \"fruit\" : \"orange\" }");
+    contextRunner(BaseConfiguration.class, "foo.json.enabled=true", "foo.json.fieldName=fruit")
+        .run(context ->
+            assertThat(context)
+                .getBean(FooService.class)
+                .isInstanceOf(JsonFooService.class)
+                .satisfies(bean -> {
+                  bean.doSomething("orange");
+                  verify(out, times(1)).println("{ \"fruit\" : \"orange\" }");
+                })
+        );
   }
 
   @Test
   public void should_configure_userFooService() {
-    load(UserConfiguration.class);
-
-    FooService bean = context.getBean(FooService.class);
-    bean.doSomething("bar");
-
-    assertThat(bean).isInstanceOf(UserFooService.class);
-    verify(out, times(1)).println("bar");
+    contextRunner(UserConfiguration.class)
+        .run(context ->
+            assertThat(context)
+                .getBean(FooService.class)
+                .isInstanceOf(UserFooService.class)
+                .satisfies(bean -> {
+                  bean.doSomething("bar");
+                  verify(out, times(1)).println("bar");
+                })
+        );
   }
 
-  private void load(Class<?> config, String... environment) {
-    context.register(config);
-    EnvironmentTestUtils.addEnvironment(context, environment);
-    context.refresh();
+  private ApplicationContextRunner contextRunner(Class<?> config, String... environment) {
+    return new ApplicationContextRunner()
+        .withConfiguration(AutoConfigurations.of(config))
+        .withPropertyValues(environment);
   }
 
   @Configuration
