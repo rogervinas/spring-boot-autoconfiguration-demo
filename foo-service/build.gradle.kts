efc8b70b2d67b808008f533ef7da14af67746b00
Roger Viñas Alcon <roger.vinas@fastzink.com>
2023-11-13 09:52:32 +0100

    Update build.gradle.kts

--- foo-service/build.gradle.kts
+++ foo-service/build.gradle.kts
@@ -3,7 +3,8 @@ plugins {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-bom:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.mockito:mockito-core:4.2.0")
 }
